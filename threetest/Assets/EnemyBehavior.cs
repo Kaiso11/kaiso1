e10bbdbd2a037c0705e2ecc83e27b26305d2c233
Kaiso11 <wakamen.jp@gmail.com>
2017-02-25 22:10:38 +0900

    敵の挙動について書き始めた

---
+++ threetest/Assets/EnemyBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    //敵のステータス、思考パターンについてをここに
+    //基本的にメタの子として生まれ、時間について、列についての情報を与えられることにより動作する
+
+
+    private float lars;//憤怒
+    private float fru;//苛立ち
+    private float md;//めんどくささ
+    private 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
